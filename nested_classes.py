cd351cda0068f63b2a59da93cdab1f1f3c53c157
KaynBelmont <caio.vinicius115@hotmail.com>
2026-03-04 11:52:24 -0300

    Nested Classes

---
+++ nested_classes.py
@@ -0,0 +1,37 @@
+#Criar a classe main
+
+class Computador:
+    def __init__(self, modelo, gpu_nome, gpu_memoria, cpu_nome, cpu_cores, cpu_clock):
+        self.modelo = modelo
+        self.gpu = self.GPU(gpu_nome, gpu_memoria)
+        self.cpu = self.CPU(cpu_nome, cpu_cores, cpu_clock)
+
+
+    def mostrar_config(self):
+        print(f'Computador: {self.modelo}')
+        self.gpu.mostrar_gpu()
+        self.cpu.mostrar_cpu()
+
+    class GPU: #NESTED CLASS
+        def __init__(self, nome, memoria_gb):
+            self.nome = nome
+            self.memoria_gb = memoria_gb
+
+        def mostrar_gpu(self):
+            print(f'GPU: {self.nome} - {self.memoria_gb}GB')
+
+    class CPU: #NESTED CLASS
+        def __init__(self, nome, cores, clock_ghz):
+            self.nome = nome
+            self.cores = cores
+            self.clock_ghz = clock_ghz
+
+        def mostrar_cpu(self):
+            print(f'CPU: {self.nome} - {self.cores} cores - {self.clock_ghz}GHz')
+
+
+
+
+#Utilização
+pc1 = Computador('Razer', 'NVIDIA RTX 3060', 12, 'AMD Ryzen 5500', 10, 3.5)
+pc1.mostrar_config()
